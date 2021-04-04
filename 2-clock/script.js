8cbdddac62d1314237d3ae01e050718f4c3e276e
Moomal <moomalmsoomro@gmail.com>
2021-04-04 19:30:57 -0400

    Add button functions

---
+++ 2-clock/script.js
@@ -0,0 +1,19 @@
+function sizeUp() {
+	
+	document.getElementById("clockbox").style.fontSize = "7vw";
+}
+
+function sizeDown() {
+
+	document.getElementById("clockbox").style.fontSize = "1vw";
+}
+
+function changeColor() {
+
+	document.getElementById("clockbox").style.color = "red";
+}
+
+function addShadow() {
+
+	document.getElementById("clockbox").style.textShadow = "5px 5px green";
+}

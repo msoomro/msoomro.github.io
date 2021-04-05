48c2ec9b23c4e1ebcd45496a8d95e61cb42a4f40
Moomal <moomalmsoomro@gmail.com>
2021-04-05 04:30:45 -0400

    Allow images to alternate between visible and hidden with each click

--- 4-butterflies/app.js
+++ 4-butterflies/app.js
@@ -1,6 +1,14 @@
 function toggleImage(element, num) {
-	element.innerHTML = "";
-	element.style.background = `url('images/butterfly${num}.jpg')`;
-	element.style.backgroundSize = "cover";
-	element.style.opacity = '0.95';
+	if(element.innerHTML != "")
+	{
+		element.innerHTML = "";
+		element.style.background = `url('images/butterfly${num}.jpg')`;
+		element.style.backgroundSize = "cover";
+		element.style.opacity = '0.95';
+	}
+	else if(element.innerHTML == "")
+	{
+		element.style.background = 'rgba(234,234,234,.7)';
+		element.innerHTML = "&#129419";
+	}
 }
